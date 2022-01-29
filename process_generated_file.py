309f0c2277a197daa6f5139354305b8e0569ca3b
swarnveer <singh.swarnveer@gmail.com>
2022-01-30 00:36:53 +0530

    Update process_generated_file.py

--- process_generated_file.py
+++ process_generated_file.py
@@ -7,11 +7,12 @@ def process_excel(df):
     df.loc[df["Order Type"].str.contains("Delivered|Dispatched|Replace"), "Order Type"] = "Product"
     df.loc[df["Order Type"].str.contains("Return|Refund"), "Order Type"] = "Order returned"
     df.loc[df["Order Type"].str.contains("Successful"), "Order Type"] = "Bill"
-    df.loc[df["Order Type"].str.contains("Order returned"), "Order Value"] *= -1
     df['Order Year'] = pd.DatetimeIndex(df['Order Date']).year
+    df_filtered = df[(df["Order Type"] != 'Order returned')]
 
-    df2=df
+    df2=df_filtered
     df2 = df2.groupby('Order Year').sum()
     df2['Order Year'] = df2.index
+    df.to_excel("Amazon.xlsx", index=False)
     df['Order Value'] = abs(df['Order Value'])
-    show_graph(df,df2)
+    show_graph(df_filtered,df2)
